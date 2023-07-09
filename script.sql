863f55e64df22d82afd6aa35df1e64d608a0b8c4
yogesh-patil-27 <138602918+yogesh-patil-27@users.noreply.github.com>
2023-07-09 18:44:18 +0530

    script.sql

---
+++ script.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `userdb`,`users`(
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NULL,
+  `country` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
